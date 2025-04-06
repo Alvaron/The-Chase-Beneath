9de4cdc528aae5946c7e8153023701ca3b338151
Joris Pyl <joris.pyl@gmail.Com>
2025-04-07 01:27:14 +0200

    credits
    made sure to include credits for the assets used (before I forget where I got them)

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,12 +1,25 @@
 extends Node3D
 
+@onready var menu_display: VBoxContainer = $MarginContainer/VBoxContainer
+@onready var credits_display: VBoxContainer = $MarginContainer/VBoxContainer2
+
+func _ready() -> void:
+	menu_display.visible = true
+	credits_display.visible = false
+
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/test.tscn")
 
 
 func _on_credits_pressed() -> void:
-	pass # Replace with function body.
+	menu_display.visible = false
+	credits_display.visible = true
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
+
+
+func _on_button_pressed() -> void:
+	menu_display.visible = true
+	credits_display.visible = false
